8848e090f98cc6dd5c76f8c9c073b60715364a12
luchbl <hoangbaluc1005@gmail.com>
2023-01-03 10:33:01 +0700

    them product-detail

--- scss/js/flickity-install.js
+++ scss/js/flickity-install.js
@@ -30,31 +30,36 @@ Flickity.prototype.resize = function () {
         });
     });
     // products detail - gallery vertical
-    if ($('.carousel-nav').length || $('.carousel-main').length) {
-        var $carousel = $('.carousel-main').flickity();
-        var $carouselNav = $('.carousel-nav');
-        var $carouselNavCells = $carouselNav.find('.carousel-cell');
-        $carouselNav.on('click', '.carousel-cell', function(event) {
-            var index = $(event.currentTarget).index();
-            $carousel.flickity('select', index);
-        });
-        var flkty = $carousel.data('flickity');
-        var navTop = $carouselNav.position().top;
-        var navCellHeight = $carouselNavCells.height();
-        var navHeight = $carouselNav.height();
+    // if ($('.carousel-nav').length || $('.carousel-main').length) {
+    //     var $carousel = $('.carousel-main').flickity();
+    //     var $carouselNav = $('.carousel-nav');
+    //     var $carouselNavCells = $carouselNav.find('.carousel-cell');
+    //     $carouselNav.on('click', '.carousel-cell', function(event) {
+    //         var index = $(event.currentTarget).index();
+    //         $carousel.flickity('select', index);
+    //     });
+    //     var flkty = $carousel.data('flickity');
+    //     var navTop = $carouselNav.position().top;
+    //     var navCellHeight = $carouselNavCells.height();
+    //     var navHeight = $carouselNav.height();
 
-        $carousel.on('select.flickity', function() {
-            // set selected nav cell
-            $carouselNav.find('.is-nav-selected').removeClass('is-nav-selected');
-            var $selected = $carouselNavCells.eq(flkty.selectedIndex)
-                .addClass('is-nav-selected');
-            // scroll nav
-            var scrollY = $selected.position().top +
-                $carouselNav.scrollTop() - (navHeight + navCellHeight) / 2;
-            $carouselNav.animate({
-                scrollTop: scrollY
-            });
-        });
-    }
-    
+    //     $carousel.on('select.flickity', function() {
+    //         // set selected nav cell
+    //         $carouselNav.find('.is-nav-selected').removeClass('is-nav-selected');
+    //         var $selected = $carouselNavCells.eq(flkty.selectedIndex)
+    //             .addClass('is-nav-selected');
+    //         // scroll nav
+    //         var scrollY = $selected.position().top +
+    //             $carouselNav.scrollTop() - (navHeight + navCellHeight) / 2;
+    //         $carouselNav.animate({
+    //             scrollTop: scrollY
+    //         });
+    //     });
+    // }
+    $('.carousel-main').flickity();
+    $('.carousel-nav').flickity({
+        asNavFor: '.carousel-main',
+        contain: true,
+        pageDots: false
+    });    
 })(jQuery);
